b2e5d84393bbeb94614865ec79d2d73e1df6f16e
tsingsonqin <tsingson@gmail.com>
2018-05-09 04:22:22 +0800

    fix postGrest DELETE action
    fix postGrest DELETE action:
    
    delete one item or delete multiple item with parames.ids

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,79 +1,71 @@
 import axios from "axios";
 import {
-    AUTH_LOGIN,
-    AUTH_LOGOUT,
-    AUTH_ERROR,
-    AUTH_CHECK,
-    AUTH_GET_PERMISSIONS
+	AUTH_CHECK,
+	AUTH_ERROR,
+	AUTH_GET_PERMISSIONS,
+	AUTH_LOGIN,
+	AUTH_LOGOUT
 } from "react-admin";
-
-export default (type, params) => {
-    // logout
-    if (type === AUTH_LOGOUT) {
-        //	localStorage.removeItem("username");
-        localStorage.removeItem("token");
-        return Promise.resolve();
-    }
-    // auth error
-    if (type === AUTH_ERROR) {
-        const status = params.status;
-        if (status !== 200) {
-            //	localStorage.removeItem("username");
-            localStorage.removeItem("token");
-            return Promise.reject();
-        }
-        return Promise.resolve();
-    }
-    // login
-    if (type === AUTH_LOGIN) {
-        const { username, password } = params;
-        let url = "http://localhost:3002/rpc/login";
-        //
-        axios.defaults.headers.post["Content-Type"] = "application/json";
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-                Accept: "application/json"
-            },
-            data: `{"username":"${username}","password":"${password}"}`,
-            url: url
-        };
-        //
-        axios(options)
-            .then(response => {
-                if (response.status === 200) {
-                    let token = response.data[0].token;
-                    response.data = { token: token };
-                    //		localStorage.setItem("username", username);
-                    localStorage.setItem("token", token);
-                    return response;
-                }
-                return response;
-            })
-            .catch(error => {
-                //	console.error(error);
-                return Promise.reject(error);
-            });
-        return Promise.resolve();
-    }
-    // called when the user navigates to a new location
-    if (type === AUTH_CHECK) {
-        return localStorage.getItem("token")
-            ? Promise.resolve()
-            : Promise.reject();
-    }
-
-    /**
-     if (type === AUTH_GET_PERMISSIONS) {
-		const token = localStorage.getItem("token");
-		if (!token) {
+// var authUrl = "http://localhost:3002/rpc/login";
+export default authUrl => (type, params) => {
+	// logout
+	if (type === AUTH_LOGOUT) {
+		localStorage.removeItem("token");
+		return Promise.resolve();
+	}
+	// auth error
+	if (type === AUTH_ERROR) {
+		const status = params.status;
+		if (status !== 200) {
+			localStorage.removeItem("token");
 			return Promise.reject();
 		}
-		const role = localStorage.getItem("role");
-		Promise.resolve(role);
+		return Promise.resolve();
 	}
-     */
-    //
-    return Promise.reject("Unknown method");
+	// login
+	if (type === AUTH_LOGIN) {
+		const { username, password } = params;
+		//	let url = "http://localhost:3002/rpc/login";
+		axios.defaults.headers.post["Content-Type"] = "application/json";
+		const axiosOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json;charset=utf-8",
+				Accept: "application/json"
+			},
+			data: `{"username":"${username}","password":"${password}"}`,
+			url: authUrl
+		};
+		//
+		axios(axiosOptions).then(response => {
+			if (response.status === 200) {
+				let token = response.data[0].token;
+				response.data = { token: token };
+				// save token
+				localStorage.setItem("token", token);
+				return response;
+			}
+			return response;
+		});
+		/**
+             .catch((error) => {
+                    return Promise.reject(error);
+                });
+             */
+		return Promise.resolve();
+	}
+	// called when the user navigates to a new location
+	if (type === AUTH_CHECK) {
+		return localStorage.getItem("token")
+			? Promise.resolve()
+			: Promise.reject();
+	}
+	// get permissions
+	if (type === AUTH_GET_PERMISSIONS) {
+		//     const role = localStorage.getItem('role');
+		//     return role ? Promise.resolve(role) : Promise.reject();
+		return Promise.resolve();
+	}
+
+	return Promise.reject("Unknown method");
 };
